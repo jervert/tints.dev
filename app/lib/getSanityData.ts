b9fc25de8b44c12c7cb1ce0ded0272170e2df76e
Antonio Rodríguez <arr2019@protonmail.ch>
2025-11-12 00:29:08 +0100

    content pending

--- app/lib/getSanityData.ts
+++ app/lib/getSanityData.ts
@@ -1,19 +1,15 @@
-export const config = {
-  projectId: `az8av6xl`,
-  apiVersion: `v2021-03-25`,
-  dataset: `production`,
-};
-
 export async function getSanityData() {
-  // Some **lazy** Sanity fetchin'
-  const { projectId, apiVersion, dataset } = config;
-
-  const query = `*[_id == "tailwind"][0].content`;
-  const about = await fetch(
-    `https://${projectId}.apicdn.sanity.io/${apiVersion}/data/query/${dataset}?query=${query}`,
-  )
-    .then((res) => res.json())
-    .catch(() => null);
+  const about = {
+    result: [
+      {
+        _key: "",
+        _type: "block",
+        children: [
+          { _key: "", _type: "span", marks: [], text: "Content pending" },
+        ],
+      },
+    ],
+  };
 
   return about?.result;
 }
